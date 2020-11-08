8d5b409b887ba5da9531fd412512021039f1a42d
Jiho Yeo <jiho@kaist.ac.kr>
2020-11-08 14:51:28 +0900

    Update intra-zonal relocation module for Strategy 1

--- module/utils/TotalNumVeh.R
+++ module/utils/TotalNumVeh.R
@@ -5,9 +5,11 @@ TotalNumVeh <- function(time){
   print (paste("Idle is", nrow(IdleVehicle[[time]])))
   print (paste("Assign is", nrow(AssignVehicle[[time]])))
   print (paste("Oprt is", nrow(OprtVehicle[[time]])))
-  print (paste("Relocate is", nrow(InterReloVehicle[[time]])))
+  print (paste("Inter-Relocate is", nrow(InterReloVehicle[[time]])))
+  print (paste("Intra-Relocate is", nrow(IntraReloVehicle[[time]])))
   return(nrow(IdleVehicle[[time]])+
            nrow(AssignVehicle[[time]])+
            nrow(OprtVehicle[[time]])+
-           nrow(InterReloVehicle[[time]]))
+           nrow(InterReloVehicle[[time]])+
+           nrow(IntraReloVehicle[[time]]))
 }
